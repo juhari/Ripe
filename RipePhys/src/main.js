b7b21cfb68a38bfb3c1516d5828eafff823d4111
Matti Pellikka <matti.pellikka@gmail.com>
2013-07-24 14:50:56 +0300

    moving ball

--- RipePhys/src/main.js
+++ RipePhys/src/main.js
@@ -14,15 +14,22 @@ var Layer    = nodes.Layer
   , Label    = nodes.Label
   , Director = cocos.Director
 
+var ControlEnum = {
+    LEFT : -1,
+    RIGHT: 1,
+    NEUTRAL: 0
+}
 
 function PhysicsDemo () {
     PhysicsDemo.superclass.constructor.call(this)
 
     this.isMouseEnabled = true
+    this.isKeyboardEnabled = true
 
 
     this.bodies = []
-
+    this.wheels = []
+    this.control = ControlEnum.NEUTRAL
 
     // Get size of canvas
     var s = Director.sharedDirector.winSize
@@ -37,6 +44,10 @@ PhysicsDemo.inherit(Layer, {
     bodies: null,
     selectedBody: null,
     mouseJoint: null,
+    wheels: null,
+    control: null,
+    rigthFlag: null,
+    leftFlag: null,
 
     createCrate: function (point, scale) {
         scale = scale || 1
@@ -60,9 +71,52 @@ PhysicsDemo.inherit(Layer, {
         return sprite
     },
 
+    computeTorque: function(omega, rightFlag, leftFlag) {
+        /*if(rightFlag === 1) { 
+            return -5
+        }
+        if(leftFlag === 1) { 
+            return 5
+        }*/
+
+        var k = 1
+        var b = 5
+        var torque = 0
+
+        if(omega < 0) {
+            if(rightFlag === 1) { 
+                torque = -(k*omega + b)
+            }
+            if(leftFlag === 1) { 
+                torque = b 
+            }
+
+        }
+        else {
+            if(rightFlag === 1) { 
+                torque = -b
+            }
+            if(leftFlag === 1) { 
+                torque = k*omega + b
+            }
+        }
+
+        console.log("Torque: " + torque + " Omega: " + omega)
+        return torque
+    },
+
     update: function (dt) {
         var world = this.world,
             mouseJoint = this.mouseJoint
+
+        var wheels = this.wheels
+        for (var i = 0, len = wheels.length; i < len; i++) {
+            console.log("Apply forces")
+            var wheel = wheels[i]
+            var torque = 5//this.computeTorque(wheel.GetAngularVelocity(), 
+            //this.rightFlag, this.leftFlag)
+            wheel.ApplyTorque(torque)
+        }
 
         world.Step(dt, 10, 10)
         world.ClearForces()
@@ -73,9 +127,7 @@ PhysicsDemo.inherit(Layer, {
             pos = body.GetPosition(),
             angle = geo.radiansToDegrees(-body.GetAngle())
             body.sprite.position = new geo.Point(pos.x * 30, pos.y * 30)
-
             body.sprite.rotation = angle
-
         }
     },
 
@@ -97,127 +149,68 @@ PhysicsDemo.inherit(Layer, {
         //create ground
         bodyDef.type = box2d.b2Body.b2_staticBody
         fixDef.shape = new box2d.b2PolygonShape
+
         fixDef.shape.SetAsBox(20, 2)
         bodyDef.position.Set(10, 400 / 30 + 2)
         world.CreateBody(bodyDef).CreateFixture(fixDef)
+        
         bodyDef.position.Set(10, -2)
         world.CreateBody(bodyDef).CreateFixture(fixDef)
+
         fixDef.shape.SetAsBox(2, 14)
         bodyDef.position.Set(-2, 13)
         world.CreateBody(bodyDef).CreateFixture(fixDef)
+        
         bodyDef.position.Set(22, 13)
         world.CreateBody(bodyDef).CreateFixture(fixDef)
 
 
         //create some objects
         bodyDef.type = box2d.b2Body.b2_dynamicBody
-        for (var i = 0; i < 15; ++i) {
-            var sprite
-            bodyDef.position.x = Math.random() * 15
-            bodyDef.position.y = Math.random() * 15
-            var scale = (Math.random() + 0.5),
-                width = scale * 32
-            if (Math.random() > 0.5) {
-                fixDef.shape = new box2d.b2PolygonShape
-                fixDef.shape.SetAsBox(width/30, width/30)
-                sprite = this.createCrate(new geo.Point(bodyDef.position.x * 30, bodyDef.position.y * 30), scale)
-            } else {
-                fixDef.shape = new box2d.b2CircleShape(width/30)
-                sprite = this.createBall(new geo.Point(bodyDef.position.x * 30, bodyDef.position.y * 30), scale)
-            }
+        
+        bodyDef.position.x = 0
+        bodyDef.position.y = 5
+        
+        var scale = 0.5,
+        width = scale * 32
 
-            var bdy = world.CreateBody(bodyDef)
-            bdy.sprite = sprite
-            this.bodies.push(bdy)
-            bdy.CreateFixture(fixDef)
-        }
+        fixDef.shape = new box2d.b2CircleShape(width/30)
+        var sprite = this.createBall(new geo.Point(bodyDef.position.x * 30, bodyDef.position.y * 30), scale)
 
+        var bdy = world.CreateBody(bodyDef)
+        bdy.sprite = sprite
+        this.bodies.push(bdy)
+        this.wheels.push(bdy)
+        bdy.CreateFixture(fixDef)
 
-
-        /*
-        //setup debug draw
-        var debugDraw = new box2d.b2DebugDraw()
-            debugDraw.SetSprite(document.getElementById('debug-canvas').getContext("2d"))
-            debugDraw.SetDrawScale(30.0)
-            debugDraw.SetFillAlpha(0.5)
-            debugDraw.SetLineThickness(1.0)
-            debugDraw.SetFlags(box2d.b2DebugDraw.e_shapeBit | box2d.b2DebugDraw.e_jointBit)
-            world.SetDebugDraw(debugDraw)
-        */
+        this.rightFlag = 0
+        this.leftFlag = 0
     }, 
 
-    getBodyAtPoint: function (point) {
-        point = new geo.Point(point.x /30, point.y /30)
-        var world = this.world
-        var mousePVec = new box2d.b2Vec2(point.x, point.y)
-        var aabb = new box2d.b2AABB()
-        aabb.lowerBound.Set(point.x - 0.001, point.y - 0.001)
-        aabb.upperBound.Set(point.x + 0.001, point.y + 0.001)
-
-
-        var self = this
-        function getBodyCB(fixture) {
-            if(fixture.GetBody().GetType() != box2d.b2Body.b2_staticBody) {
-                if(fixture.GetShape().TestPoint(fixture.GetBody().GetTransform(), mousePVec)) {
-                    self.selectedBody = fixture.GetBody()
-
-                    return false
-                }
-            }
-            return true
+    keyUp: function(evt) {
+        console.log(evt)
+        if (evt.keyCode === 39) {
+            console.log("rightFlag disabled")
+            this.rightFlag = 0         
         }
-
-
-        // Query the world for overlapping shapes.
-
-        this.selectedBody = null
-
-        world.QueryAABB(getBodyCB, aabb)
-        return this.selectedBody
-    },
-
-    mouseDown: function (evt) {
-        var point = evt.locationInCanvas,
-        world = this.world,
-            mouseJoint = this.mouseJoint
-
-        if (!mouseJoint) {
-            var body = this.getBodyAtPoint(point)
-            if(body) {
-                var md = new box2d.b2MouseJointDef()
-                md.bodyA = world.GetGroundBody()
-                md.bodyB = body
-                md.target.Set(point.x /30, point.y /30)
-                md.collideConnected = true
-                md.maxForce = 300.0 * body.GetMass()
-                mouseJoint = world.CreateJoint(md)
-                body.SetAwake(true)
-                this.mouseJoint = mouseJoint
-
-            }
+        if (evt.keyCode === 37) {
+            console.log("leftFlag disabled")
+            this.leftFlag = 0
         }
     },
 
-    mouseDragged: function (evt) {
-        var point = evt.locationInCanvas,
-        world = this.world,
-            mouseJoint = this.mouseJoint
-
-        if (mouseJoint) {
-            mouseJoint.SetTarget(new box2d.b2Vec2(point.x /30, point.y /30))
+    keyDown: function(evt) {
+        console.log(evt)
+        if (evt.keyCode === 39) {
+            console.log("rightFlag enabled")
+            this.rightFlag = 1
         }
-    },
-
-    mouseUp: function (evt) {
-        var mouseJoint = this.mouseJoint,
-            world = this.world
-
-        if (mouseJoint) {
-            world.DestroyJoint(mouseJoint)
-            this.mouseJoint = null
-
+        if (evt.keyCode === 37) {
+            console.log("leftFlag enabled")
+            this.leftFlag = 1
         }
     }
+
 })
 
 /**
