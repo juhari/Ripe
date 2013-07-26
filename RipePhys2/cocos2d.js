3acb5b58655e7df8b92c33b05269c5691a6849a1
Matti Pellikka <matti.pellikka@gmail.com>
2013-07-26 12:18:38 +0300

    Added cocos2d-html5 engine

--- RipePhys2/cocos2d.js
+++ RipePhys2/cocos2d.js
@@ -35,7 +35,7 @@
         loadExtension:false,
         renderMode:0,       //Choose of RenderMode: 0(default), 1(Canvas only), 2(WebGL only)
         tag:'gameCanvas', //the dom element to run cocos2d on
-        engineDir:'../../cocos2d-html5/cocos2d/',
+        engineDir:'../Cocos2d-html5-v2.1.4/cocos2d/',
         //SingleEngineFile:'',
         appFiles:[
             'src/resource.js',
