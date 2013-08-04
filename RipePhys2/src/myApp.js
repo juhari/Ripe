dbae11a40d72060281d0791189846a6904ea055f
Juha Riippi <juha.riippi@vincit.fi>
2013-08-04 17:42:48 +0300

    Some refactoring and cleanup

--- RipePhys2/src/myApp.js
+++ RipePhys2/src/myApp.js
@@ -15,7 +15,7 @@ var Helloworld = cc.Layer.extend({
     selectedBody: null,
     mouseJoint: null,
     centerBody: null,
-    rigthFlag: null,
+    rightFlag: null,
     leftFlag: null,
 
     init:function () {
@@ -47,24 +47,24 @@ var Helloworld = cc.Layer.extend({
         /////////////////////////////
         // 3. add your codes below...
 
-        this.bodies = []
-        this.wheels = []
+        this.bodies = [];
+        this.wheels = [];
 
         this.setMouseEnabled(true);
         this.setKeyboardEnabled(true);
         this.setTouchEnabled(true);
 
-        this.demo()
+        this.demo();
 
-        this.scheduleUpdate()
+        this.scheduleUpdate();
 
         return true;
     },
 
     createGroundSprite: function(point, bodyWidth, bodyHeight) {
 
-        var sprite = cc.Sprite.create("res/asfalt.jpg")
-        sprite.setPosition(point)
+        var sprite = cc.Sprite.create("res/asfalt.jpg");
+        sprite.setPosition(point);
 
         var spriteSize = sprite.getTextureRect();
         var bodyWidthPixels = bodyWidth*2*pixelsPerMeter;
@@ -72,26 +72,26 @@ var Helloworld = cc.Layer.extend({
         sprite.setScaleY(bodyHeightPixels/spriteSize.height);
         sprite.setScaleX(bodyWidthPixels/spriteSize.width);
 
-        this.addChild(sprite)
+        this.addChild(sprite);
         return sprite
     },
 
     createBall: function (point, scale) {
-        scale = scale || 1
-        var sprite = cc.Sprite.create("res/ball.png")
-        sprite.setPosition(point)
+        scale = scale || 1;
+        var sprite = cc.Sprite.create("res/ball.png");
+        sprite.setPosition(point);
 
-        sprite.setScale(scale)
+        sprite.setScale(scale);
 
-        this.addChild(sprite)
+        this.addChild(sprite);
         return sprite
     },
 
     computeTorque: function(omega, rightFlag, leftFlag) {
-        var k = 1
+        var k = 1;
         var enginePower = 45;
         var brakePower = 5;
-        var torque = 0
+        var torque = 0;
 
         if(omega < 0) {
             if(rightFlag === 1) { 
@@ -117,131 +117,130 @@ var Helloworld = cc.Layer.extend({
 
     update: function (dt) {
         var world = this.world,
-        mouseJoint = this.mouseJoint        
+            mouseJoint = this.mouseJoint;
 
-        var wheels = this.wheels
+        var wheels = this.wheels;
         for (var i = 0, len = wheels.length; i < len; i++) {
-            var wheel = wheels[i]
-            var torque = this.computeTorque(wheel.GetAngularVelocity(), this.rightFlag, this.leftFlag)
+            var wheel = wheels[i];
+            var torque = this.computeTorque(wheel.GetAngularVelocity(), this.rightFlag, this.leftFlag);
             wheel.ApplyTorque(torque)
         }
 
-        world.Step(dt, 10, 10)
-        world.ClearForces()
+        world.Step(dt, 10, 10);
+        world.ClearForces();
         
         
-        var pos = this.centerBody.GetPosition()
-        var scenePos = new cc.Point(-pos.x * pixelsPerMeter + 
-                                    this.getContentSize().width/2,
-                                    -pos.y * pixelsPerMeter + 
-                                    this.getContentSize().height/2)        
-        this.setPosition(scenePos)
+        var pos = this.centerBody.GetPosition();
+        var scenePos = new cc.Point(-pos.x * pixelsPerMeter +
+            this.getContentSize().width / 2,
+            -pos.y * pixelsPerMeter +
+                this.getContentSize().height / 2);
+        this.setPosition(scenePos);
 
-        var bodies = this.bodies
+        var bodies = this.bodies;
         for (var i = 0, len = bodies.length; i < len; i++) {
             var body = bodies[i],
-            pos = body.GetPosition(),
-            angle = cc.RADIANS_TO_DEGREES(-body.GetAngle()),
-            point = new cc.Point(pos.x * pixelsPerMeter, 
-                                 pos.y * pixelsPerMeter)
-            body.sprite.setPosition(point)
-            body.sprite.setRotation(angle)
+                pos = body.GetPosition(),
+                angle = cc.RADIANS_TO_DEGREES(-body.GetAngle()),
+                point = new cc.Point(pos.x * pixelsPerMeter,
+                    pos.y * pixelsPerMeter);
+            body.sprite.setPosition(point);
+            body.sprite.setRotation(angle);
         }
     },
 
     demo: function () {
-        var world = new b2World(
+        this.world = new b2World(
             new b2Vec2(0, -10),    //gravity
             true                  //allow sleep
-        )
-        this.world = world
+        );
 
-        var fixDef = new b2FixtureDef
-        fixDef.density = 1.0
-        fixDef.friction = 1.5
-        fixDef.restitution = 0.2
+        var fixDef = new b2FixtureDef;
+        fixDef.density = 1.0;
+        fixDef.friction = 1.5;
+        fixDef.restitution = 0.2;
 
-        var bodyDef = new b2BodyDef
+        this.createGround(fixDef);
+        this.createVehicle(fixDef);
 
-
-        this.createGround(fixDef, bodyDef, world)
-
-        
-        //create some objects
-        bodyDef.type = b2Body.b2_dynamicBody
-        
-        bodyDef.position.x = 0
-        bodyDef.position.y = 5
-        
-        var scale = 0.5,
-        width = scale * 32
-
-        fixDef.shape = new b2CircleShape(width/pixelsPerMeter)
-        var sprite = this.createBall(new cc.Point(bodyDef.position.x * pixelsPerMeter, bodyDef.position.y * pixelsPerMeter), scale)
-
-        var bdy = world.CreateBody(bodyDef)
-        bdy.sprite = sprite
-        this.bodies.push(bdy)
-        this.wheels.push(bdy)
-        this.centerBody = bdy
-        bdy.CreateFixture(fixDef)
-
-        this.rightFlag = 0
+        this.rightFlag = 0;
         this.leftFlag = 0
     },
 
-    createGround: function(fixDef, bodyDef, world) {
+    createVehicle: function(fixDef) {
+        var bodyDef = new b2BodyDef;
 
+        bodyDef.type = b2Body.b2_dynamicBody;
+
+        bodyDef.position.x = 0;
+        bodyDef.position.y = 5;
+
+        var scale = 0.5,
+            width = scale * 32;
+
+        fixDef.shape = new b2CircleShape(width/pixelsPerMeter);
+        var sprite = this.createBall(new cc.Point(bodyDef.position.x * pixelsPerMeter, bodyDef.position.y * pixelsPerMeter), scale);
+
+        var bdy = this.world.CreateBody(bodyDef);
+        bdy.sprite = sprite;
+        this.bodies.push(bdy);
+        this.wheels.push(bdy);
+        this.centerBody = bdy;
+        bdy.CreateFixture(fixDef)
+    },
+
+    createGround: function(fixDef) {
+
+        var bodyDef = new b2BodyDef;
         var body;
-        var scale = 1.0
 
-        var sprite
+        var sprite;
 
-        bodyDef.type = b2Body.b2_staticBody
-        fixDef.shape = new b2PolygonShape
-        fixDef.shape.SetAsBox(20, 2)
+        bodyDef.type = b2Body.b2_staticBody;
+        fixDef.shape = new b2PolygonShape;
+        fixDef.shape.SetAsBox(20, 2);
 
-        bodyDef.position.Set(10, 2)
-        body = world.CreateBody(bodyDef).CreateFixture(fixDef)
-        sprite = this.createGroundSprite(new cc.Point(bodyDef.position.x * pixelsPerMeter, bodyDef.position.y * pixelsPerMeter), 20, 2)
-        body.sprite = sprite
+        bodyDef.position.Set(10, 2);
+        body = this.world.CreateBody(bodyDef).CreateFixture(fixDef);
+        sprite = this.createGroundSprite(new cc.Point(bodyDef.position.x * pixelsPerMeter, bodyDef.position.y * pixelsPerMeter), 20, 2);
+        body.sprite = sprite;
 
-        fixDef.shape.SetAsBox(2, 14)
-        bodyDef.position.Set(-2, 13)
-        body = world.CreateBody(bodyDef).CreateFixture(fixDef)
+        fixDef.shape.SetAsBox(2, 14);
+        bodyDef.position.Set(-2, 13);
+        this.world.CreateBody(bodyDef).CreateFixture(fixDef);
 
-        bodyDef.position.Set(23.5, 13)
-        body = world.CreateBody(bodyDef).CreateFixture(fixDef)
+        bodyDef.position.Set(23.5, 13);
+        this.world.CreateBody(bodyDef).CreateFixture(fixDef);
 
         fixDef.shape.SetAsOrientedBox(5, 5, new b2Vec2(0, 0), Math.PI/4);
         bodyDef.position.Set(10, 4);
-        body = world.CreateBody(bodyDef).CreateFixture(fixDef);
-        sprite = this.createGroundSprite(new cc.Point((bodyDef.position.x) * pixelsPerMeter, bodyDef.position.y * pixelsPerMeter), 5, 5)
+        body = this.world.CreateBody(bodyDef).CreateFixture(fixDef);
+        sprite = this.createGroundSprite(new cc.Point((bodyDef.position.x) * pixelsPerMeter, bodyDef.position.y * pixelsPerMeter), 5, 5);
         sprite.setRotation(-cc.RADIANS_TO_DEGREES(Math.PI/4));
         body.sprite = sprite
     },
 
     onKeyUp: function(keycode) {
 
-        console.log(keycode)
+        console.log(keycode);
         if (keycode === 39) {
-            console.log("rightFlag disabled")
+            console.log("rightFlag disabled");
             this.rightFlag = 0         
         }
         if (keycode === 37) {
-            console.log("leftFlag disabled")
+            console.log("leftFlag disabled");
             this.leftFlag = 0
         }
     },
 
     onKeyDown: function(keycode) {
-        console.log(keycode)
+        console.log(keycode);
         if (keycode === 39) {
-            console.log("rightFlag enabled")
+            console.log("rightFlag enabled");
             this.rightFlag = 1
         }
         if (keycode === 37) {
-            console.log("leftFlag enabled")
+            console.log("leftFlag enabled");
             this.leftFlag = 1
         }
     },
