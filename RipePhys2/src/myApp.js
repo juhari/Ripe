ec235b0298e3d43fcbdf926ced46d73f90f6a92d
Juha Riippi <juha.riippi@vincit.fi>
2013-08-18 15:17:54 +0300

    New car model. Controls do not work yet

--- RipePhys2/src/myApp.js
+++ RipePhys2/src/myApp.js
@@ -6,7 +6,10 @@ var b2Vec2 = Box2D.Common.Math.b2Vec2
 , b2FixtureDef = Box2D.Dynamics.b2FixtureDef
 , b2World = Box2D.Dynamics.b2World
 , b2PolygonShape = Box2D.Collision.Shapes.b2PolygonShape
-, b2CircleShape = Box2D.Collision.Shapes.b2CircleShape;
+, b2CircleShape = Box2D.Collision.Shapes.b2CircleShape
+, b2RevoluteJointDef = Box2D.Dynamics.Joints.b2RevoluteJointDef
+, b2PrismaticJointDef = Box2D.Dynamics.Joints.b2PrismaticJointDef;
+
 
 var Helloworld = cc.Layer.extend({
     world:null,
@@ -47,7 +50,6 @@ var Helloworld = cc.Layer.extend({
         /////////////////////////////
         // 3. add your codes below...
 
-        this.bodies = [];
         this.wheels = [];
 
         this.setMouseEnabled(true);
@@ -125,32 +127,185 @@ var Helloworld = cc.Layer.extend({
         fixDef.friction = 1.5;
         fixDef.restitution = 0.2;
 
-        this.createVehicle(fixDef);
+        this.createGround();
+        //this.createGround(fixDef);
+        this.createVehicle();
 
         this.rightFlag = 0;
         this.leftFlag = 0
     },
 
-    createVehicle: function(fixDef) {
-        var bodyDef = new b2BodyDef;
+    createVehicle: function() {
 
-        bodyDef.type = b2Body.b2_dynamicBody;
+        /*
+        // Debug wheel
+        //***************************************************************
 
-        bodyDef.position.x = 0;
-        bodyDef.position.y = 5;
+        // fixture
+        var wheelFixture = new b2FixtureDef;
+        wheelFixture.density = 1.0;
+        wheelFixture.friction = 1.5;
+        wheelFixture.restitution = 0.2;
 
-        var scale = 0.5,
-            width = scale * 32;
+        // body definition
+        var wheelBodyDef = new b2BodyDef;
+        wheelBodyDef.type = b2Body.b2_dynamicBody;
+        wheelBodyDef.position.x = 0;
+        wheelBodyDef.position.y = 5;
 
-        fixDef.shape = new b2CircleShape(width/pixelsPerMeter);
-        
-        var bdy = this.world.CreateBody(bodyDef);
-        var renderer = new DebugShapeRenderer(bdy);
+        wheelFixture.shape = new b2CircleShape(16/pixelsPerMeter);
+
+        var wheelBody = this.world.CreateBody(wheelBodyDef);
+        this.wheels.push(wheelBody);
+        this.centerBody = wheelBody;
+        wheelBody.CreateFixture(wheelFixture);
+
+        // rendering
+        var renderer = new DebugShapeRenderer(wheelBody);
         this.addChild(renderer);
-        this.bodies.push(bdy);
-        this.wheels.push(bdy);
-        this.centerBody = bdy;
-        bdy.CreateFixture(fixDef)
+        */
+
+        var worldScale = pixelsPerMeter;
+
+        // ************************ THE CAR ************************ //
+        // shape
+        var carShape = new b2PolygonShape();
+        carShape.SetAsBox(120/worldScale,20/worldScale);
+        // fixture
+        var carFixture = new b2FixtureDef();
+        carFixture.density=5;
+        carFixture.friction=3;
+        carFixture.restitution=0.3;
+        carFixture.filter.groupIndex=-1;
+        carFixture.shape=carShape;
+        // body definition
+        var carBodyDef = new b2BodyDef();
+        carBodyDef.type=b2Body.b2_dynamicBody;
+        carBodyDef.position.Set(0,5);
+        /*
+        // ************************ THE TRUNK ************************ //
+        // shape
+        var trunkShape = new b2PolygonShape();
+        trunkShape.SetAsOrientedBox(40/worldScale,40/worldScale,new b2Vec2(-80/worldScale,60/worldScale));
+        // fixture
+        var trunkFixture = new b2FixtureDef();
+        trunkFixture.density=1;
+        trunkFixture.friction=3;
+        trunkFixture.restitution=0.3;
+        trunkFixture.filter.groupIndex=-1;
+        trunkFixture.shape=trunkShape;
+        // ************************ THE HOOD ************************ //
+        // shape
+        var hoodShape = new b2PolygonShape();
+        var carVector=new b2Vec2();
+        carVector[0]=new b2Vec2(-40/worldScale,20/worldScale);
+        carVector[1]=new b2Vec2(-40/worldScale,100/worldScale);
+        carVector[2]=new b2Vec2(120/worldScale,20/worldScale);
+        hoodShape.SetAsVector(carVector,3);
+        // fixture
+        var hoodFixture = new b2FixtureDef();
+        hoodFixture.density=1;
+        hoodFixture.friction=3;
+        hoodFixture.restitution=0.3;
+        hoodFixture.filter.groupIndex=-1;
+        hoodFixture.shape=hoodShape;
+        */
+        // ************************ MERGING ALL TOGETHER ************************ //
+        // the car itself
+        var car=this.world.CreateBody(carBodyDef);
+        car.CreateFixture(carFixture);
+        //car.CreateFixture(trunkFixture);
+        //car.CreateFixture(hoodFixture);
+        this.centerBody = car;
+
+        var renderer = new DebugShapeRenderer(car);
+        this.addChild(renderer);
+
+        // ************************ THE AXLES ************************ //
+        // shape
+        /*
+        var axleShape = new b2PolygonShape();
+        axleShape.SetAsBox(20/worldScale,20/worldScale);
+        // fixture
+        var axleFixture = new b2FixtureDef();
+        axleFixture.density=0.5;
+        axleFixture.friction=3;
+        axleFixture.restitution=0.3;
+        axleFixture.shape=axleShape;
+        axleFixture.filter.groupIndex=-1;
+        // body definition
+        var axleBodyDef = new b2BodyDef();
+        axleBodyDef.type=b2Body.b2_dynamicBody;
+        // the rear axle itself
+        axleBodyDef.position.Set(car.GetWorldCenter().x-(60/worldScale),car.GetWorldCenter().y-(65/worldScale));
+        var rearAxle=this.world.CreateBody(axleBodyDef);
+        rearAxle.CreateFixture(axleFixture);
+        renderer = new DebugShapeRenderer(rearAxle);
+        this.addChild(renderer);
+
+        // the front axle itself
+        axleBodyDef.position.Set(car.GetWorldCenter().x+(75/worldScale),car.GetWorldCenter().y-(65/worldScale));
+        var frontAxle=this.world.CreateBody(axleBodyDef);
+        frontAxle.CreateFixture(axleFixture);
+
+        renderer = new DebugShapeRenderer(frontAxle);
+        this.addChild(renderer);
+        */
+        // ************************ THE WHEELS ************************ //
+        // shape
+        var wheelShape=new b2CircleShape(40/worldScale);
+        // fixture
+        var wheelFixture = new b2FixtureDef();
+        wheelFixture.density=1;
+        wheelFixture.friction=3;
+        wheelFixture.restitution=0.1;
+        wheelFixture.filter.groupIndex=-1;
+        wheelFixture.shape=wheelShape;
+        // body definition
+        var wheelBodyDef = new b2BodyDef();
+        wheelBodyDef.type=b2Body.b2_dynamicBody;
+        // the real wheel itself
+        wheelBodyDef.position.Set(car.GetWorldCenter().x-(60/worldScale),car.GetWorldCenter().y-(65/worldScale));
+        var rearWheel=this.world.CreateBody(wheelBodyDef);
+        rearWheel.CreateFixture(wheelFixture);
+        // the front wheel itself
+        wheelBodyDef.position.Set(car.GetWorldCenter().x+(75/worldScale),car.GetWorldCenter().y-(65/worldScale));
+        var frontWheel=this.world.CreateBody(wheelBodyDef);
+        frontWheel.CreateFixture(wheelFixture);
+
+        renderer = new DebugShapeRenderer(rearWheel);
+        this.addChild(renderer);
+        renderer = new DebugShapeRenderer(frontWheel);
+        this.addChild(renderer);
+
+        // ************************ REVOLUTE JOINTS ************************ //
+        // rear joint
+        /*
+        var rearWheelRevoluteJointDef=new b2RevoluteJointDef();
+        rearWheelRevoluteJointDef.Initialize(rearWheel,rearAxle,rearWheel.GetWorldCenter());
+        rearWheelRevoluteJointDef.enableMotor=true;
+        rearWheelRevoluteJointDef.maxMotorTorque=10000;
+        var rearWheelRevoluteJoint=this.world.CreateJoint(rearWheelRevoluteJointDef);
+        // front joint
+        var frontWheelRevoluteJointDef=new b2RevoluteJointDef();
+        frontWheelRevoluteJointDef.Initialize(frontWheel,frontAxle,frontWheel.GetWorldCenter());
+        frontWheelRevoluteJointDef.enableMotor=true;
+        frontWheelRevoluteJointDef.maxMotorTorque=10000;
+        var frontWheelRevoluteJoint=this.world.CreateJoint(frontWheelRevoluteJointDef);
+        // ************************ PRISMATIC JOINTS ************************ //
+        //  definition
+        var axlePrismaticJointDef=new b2PrismaticJointDef();
+        axlePrismaticJointDef.lowerTranslation=-20/worldScale;
+        axlePrismaticJointDef.upperTranslation=5/worldScale;
+        axlePrismaticJointDef.enableLimit=true;
+        axlePrismaticJointDef.enableMotor=true;
+        // front axle
+        axlePrismaticJointDef.Initialize(car,frontAxle,frontAxle.GetWorldCenter(),new b2Vec2(0,1));
+        var frontAxlePrismaticJoint=this.world.CreateJoint(axlePrismaticJointDef);
+        // rear axle
+        axlePrismaticJointDef.Initialize(car,rearAxle,rearAxle.GetWorldCenter(),new b2Vec2(0,1));
+        var rearAxlePrismaticJoint=this.world.CreateJoint(axlePrismaticJointDef);
+        */
     },
 
     createGround: function() {
